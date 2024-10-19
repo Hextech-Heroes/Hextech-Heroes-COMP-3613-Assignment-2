fc1b80d08d1b7aab0a6c1f62ca88b22af6ab02ea
Ender <98187561+EnderEysah777@users.noreply.github.com>
2024-10-18 23:59:41 -0400

    Update student.py

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -15,3 +15,10 @@ def get_student(id):
 
 def get_all_students():
     return Student.query.all()
+    
+def get_all_students_json():
+    students = Student.query.all()
+    if not students:
+        return []
+    students = [student.get_json() for student in students]
+    return students
